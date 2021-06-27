34122a6ad514f1cd8c6aaacfff077fcc8c621125
ganesh padole <ganeshpadole55@gmail.com>
2021-06-27 13:19:33 +0530

    Added Add Item method

--- CoreDataDemo/Controllers/TodoListViewController.swift
+++ CoreDataDemo/Controllers/TodoListViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    var itemArray = [Item]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +20,47 @@ class TodoListViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-       
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-        return 0
+        return itemArray.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+        
+        return cell
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+
+        var textField = UITextField()
+        
+        let alertController = UIAlertController(title: "Add New ToDoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { action in
+        
+            let item = Item(context: self.context)
+            item.title = textField.text
+            
+            self.saveItem()
+        }
+        
+        alertController.addTextField { alertTextField in
+            textField = alertTextField
+            textField.placeholder = "Add Item"
+        }
+        
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+    
+    func saveItem() {
+        do {
+            try context.save()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
