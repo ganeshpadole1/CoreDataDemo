b63576780467cb349b25115f8346b582eb67dc3f
ganesh padole <ganeshpadole55@gmail.com>
2021-06-27 13:38:14 +0530

    Added loadItems method

--- CoreDataDemo/Controllers/TodoListViewController.swift
+++ CoreDataDemo/Controllers/TodoListViewController.swift
@@ -6,43 +6,47 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
-
+    
     var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        
+        loadItems()
+        
     }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return itemArray.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
-        
+        cell.textLabel?.text = itemArray[indexPath.row].title
         return cell
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-
+        
         var textField = UITextField()
         
         let alertController = UIAlertController(title: "Add New ToDoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { action in
-        
+            
             let item = Item(context: self.context)
             item.title = textField.text
+            item.done = false
             
+            self.itemArray.append(item)
             self.saveItem()
         }
         
@@ -59,8 +63,19 @@ class TodoListViewController: UITableViewController {
         do {
             try context.save()
         } catch let error {
-            print(error.localizedDescription)
+            print(error)
         }
+        
+        tableView.reloadData()
     }
     
+    func loadItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch let error {
+            print(error)
+        }
+    }
 }
