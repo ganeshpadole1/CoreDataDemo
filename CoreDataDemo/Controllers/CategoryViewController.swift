dd1be6cd5845ff1787b5a4651742bd355dfd00cb
ganesh padole <ganeshpadole55@gmail.com>
2021-06-27 20:55:19 +0530

    Added Category View Controller

---
+++ CoreDataDemo/Controllers/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  CoreDataDemo
+//
+//  Created by ganesh padole on 27/06/21.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
